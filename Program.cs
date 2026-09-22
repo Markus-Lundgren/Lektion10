07e997bbeb2008bafdb0fc3a7df9d6308868b0e9
Markus-Lundgren <markus.lundgren.99@gmail.com>
2026-09-22 18:00:09 +0200

    feat: add classes

--- Program.cs
+++ Program.cs
@@ -1,1 +1,40 @@
-﻿Console.WriteLine("Hello, World!");
+﻿class Program
+{
+	static void Main(string[] args)
+	{
+		Ticket ticket = new Ticket("Konsert", 500);
+		StudentTicket sTicker = new StudentTicket("Konsert", 300);
+		ticket.PrintInfo();
+		sTicker.PrintInfo();
+	}
+}
+
+public class Ticket
+{
+	public string EventName { get; set; }
+	public int BasePrice { get; set; }
+
+	public Ticket(string eventName, int basePrice)
+	{
+		EventName = eventName;
+		BasePrice = basePrice;
+	}
+
+	public int GetPrice()
+	{
+		return BasePrice;
+	}
+
+	public void PrintInfo()
+	{
+		Console.WriteLine($"{EventName}: {GetPrice()} kr");
+	}
+}
+
+public class StudentTicket : Ticket
+{
+	public StudentTicket(string eventName, int basePrice) : base(eventName, basePrice)
+	{
+
+	}
+}
