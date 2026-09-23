b614d42c96c287633068b4d202601734f3bb0c65
Markus-Lundgren <markus.lundgren.99@gmail.com>
2026-09-23 11:05:27 +0200

    feat: Assignment for lesson 11 try catch added

--- Two.cs
+++ Two.cs
@@ -1,194 +1,202 @@
-// class Program
-// {
-// 	static void Main()
-// 	{
-// 		List<Contact> list = new List<Contact>();
-// 		Contact p1 = new Contact();
-// 		p1.Name = "Markus Lundgren";
-// 		p1.Phone_Number = "076 - 895 81 43";
-// 		Contact p2 = new Contact();
-// 		p2.Name = "Moa Eriksson";
-// 		p2.Phone_Number = "0706837302";
-// 		Contact p3 = new Contact();
-// 		p3.Name = "Mr. Worldwide";
-// 		p3.Phone_Number = "+46768958144";
+class Program
+{
+	static void Main()
+	{
+		List<Contact> list = new List<Contact>();
+		Contact p1 = new Contact();
+		p1.Name = "Markus Lundgren";
+		p1.Phone_Number = "076 - 895 81 43";
+		Contact p2 = new Contact();
+		p2.Name = "Moa Eriksson";
+		p2.Phone_Number = "0706837302";
+		Contact p3 = new Contact();
+		p3.Name = "Mr. Worldwide";
+		p3.Phone_Number = "+46768958144";
 
-// 		list.Add(p1);
-// 		list.Add(p2);
-// 		list.Add(p3);
-// 		while (true)
-// 		{
-// 			//Console.Clear();
-// 			Console.WriteLine("ADRESSBOK v1");
-// 			Console.WriteLine("------------");
-// 			Console.WriteLine("1) Lägg till kontakt");
-// 			Console.WriteLine("2) Lista kontakter");
-// 			Console.WriteLine("3) Sök efter kontakt");
-// 			Console.WriteLine("4) Uppdatera kontaktnamn");
-// 			Console.WriteLine("5) Avsluta");
-// 			Console.WriteLine();
-// 			int choice = MInput.GetInputAsInt("Val: ");
-// 			switch (choice)
-// 			{
-// 				case 1:
-// 					AddContact(list);
-// 					break;
-// 				case 2:
-// 					ListContacts(list);
-// 					break;
-// 				case 3:
-// 					SearchContact(list);
-// 					break;
-// 				case 4:
-// 					SearchContact(list, true);
-// 					break;
-// 				case 5:
-// 					Environment.Exit(0);
-// 					break;
+		list.Add(p1);
+		list.Add(p2);
+		list.Add(p3);
+		while (true)
+		{
+			//Console.Clear();
+			Console.WriteLine("ADRESSBOK v1");
+			Console.WriteLine("------------");
+			Console.WriteLine("1) Lägg till kontakt");
+			Console.WriteLine("2) Lista kontakter");
+			Console.WriteLine("3) Sök efter kontakt");
+			Console.WriteLine("4) Uppdatera kontaktnamn");
+			Console.WriteLine("5) Avsluta");
+			Console.WriteLine();
+			int choice = MInput.GetInputAsInt("Val: ");
+			switch (choice)
+			{
+				case 1:
+					AddContact(list);
+					break;
+				case 2:
+					ListContacts(list);
+					break;
+				case 3:
+					SearchContact(list);
+					break;
+				case 4:
+					SearchContact(list, true);
+					break;
+				case 5:
+					Environment.Exit(0);
+					break;
 
-// 				default:
-// 					Console.WriteLine("Ej ett giltigt val");
-// 					break;
-// 			}
-// 		}
-// 	}
+				default:
+					Console.WriteLine("Ej ett giltigt val");
+					break;
+			}
+		}
+	}
 
-// 	static void AddContact(List<Contact> list)
-// 	{
-// 		Contact person = new Contact();
-// 		person.Name = MInput.GetInput("Namn: ");
+	static void AddContact(List<Contact> list)
+	{
+		Contact person = new Contact();
+		//person.Name = MInput.GetInput("Namn: ");
 
-// 		while (true)
-// 		{
-// 			person.Phone_Number = person.ValidatePhoneNumber(MInput.GetInput("Telefonnummer: "));
-// 			if (person.Phone_Number == "ERROR")
-// 			{
-// 				Console.WriteLine($"Detta är inte ett giltigt nummer");
-// 				continue;
-// 			}
-// 			Console.WriteLine($"{person.Phone_Number} är ett giltigt nummer");
-// 			break;
-// 		}
+		while (true)
+		{
+			Console.Write("Namn: ");
+			try { person.Name = Console.ReadLine(); }
+			catch (ArgumentException)
+			{
+				Console.WriteLine("Namnet får inte vara tomt!");
+				continue;
+			}
+			person.Phone_Number = person.ValidatePhoneNumber(MInput.GetInput("Telefonnummer: "));
+			if (person.Phone_Number == "ERROR")
+			{
+				Console.WriteLine($"Detta är inte ett giltigt nummer");
+				continue;
+			}
+			Console.WriteLine($"{person.Phone_Number} är ett giltigt nummer");
+			break;
+		}
 
-// 		foreach (Contact c in list)
-// 		{
-// 			if (c.Phone_Number == person.Phone_Number)
-// 			{
-// 				Console.WriteLine("Kontakt finns redan för detta nummer!");
-// 				return;
-// 			}
-// 		}
-// 		list.Add(person);
-// 	}
+		foreach (Contact c in list)
+		{
+			if (c.Phone_Number == person.Phone_Number)
+			{
+				Console.WriteLine("Kontakt finns redan för detta nummer!");
+				return;
+			}
+		}
+		list.Add(person);
+	}
 
-// 	static void ListContacts(List<Contact> list)
-// 	{
-// 		int index = 1;
-// 		foreach (Contact p in list)
-// 		{
-// 			//Console.WriteLine($"{index++}. Namn: {p.Name} Telefonnummer: {p.FormatPhoneNumber(p.ValidatePhoneNumber(p.Phone_Number))} Id: {p.Id}");
-// 			Console.WriteLine(p);
-// 		}
+	static void ListContacts(List<Contact> list)
+	{
+		int index = 1;
+		foreach (Contact p in list)
+		{
+			//Console.WriteLine($"{index++}. Namn: {p.Name} Telefonnummer: {p.FormatPhoneNumber(p.ValidatePhoneNumber(p.Phone_Number))} Id: {p.Id}");
+			Console.WriteLine(p);
+		}
 
-// 		Console.WriteLine();
-// 	}
+		Console.WriteLine();
+	}
 
-// 	static void SearchContact(List<Contact> list, bool update = false)
-// 	{
-// 		string search = MInput.GetInput("Sök efter en kontakt (namn eller nummer): ");
-// 		bool found = false;
-// 		foreach (Contact c in list)
-// 		{
-// 			string contact = c.ValidatePhoneNumber(c.Phone_Number);
-// 			if (contact.Contains(c.ValidatePhoneNumber(search)))
-// 			{
-// 				found = true;
-// 				Console.WriteLine($"{c.GetPerson()}");
-// 				if (update)
-// 				{
-// 					c.Name = MInput.GetInput("Skriv in det nya nammnet: ");
-// 					Console.WriteLine($"{c.GetPerson()} Uppdaterad: {c.UpdatedAt}");
-// 				}
-// 				return;
-// 			}
-// 		}
-// 		if (!found) Console.WriteLine("Hittade ej kontakt");
-// 	}
-// }
+	static void SearchContact(List<Contact> list, bool update = false)
+	{
+		string search = MInput.GetInput("Sök efter en kontakt (namn eller nummer): ");
+		bool found = false;
+		foreach (Contact c in list)
+		{
+			string contact = c.ValidatePhoneNumber(c.Phone_Number);
+			if (contact.Contains(c.ValidatePhoneNumber(search)))
+			{
+				found = true;
+				Console.WriteLine($"{c.GetPerson()}");
+				if (update)
+				{
+					c.Name = MInput.GetInput("Skriv in det nya nammnet: ");
+					Console.WriteLine($"{c.GetPerson()} Uppdaterad: {c.UpdatedAt}");
+				}
+				return;
+			}
+		}
+		if (!found) Console.WriteLine("Hittade ej kontakt");
+	}
+}
 
-// class Entity
-// {
-// 	public int Id { get; }
-// 	public DateTime CreatedAt;
-// 	public DateTime UpdatedAt { get; private set; }
-// 	public Entity()
-// 	{
-// 		Id = Random.Shared.Next(1, 1000000);
-// 		CreatedAt = DateTime.Now;
-// 	}
-// 	public void SetUpdatedAt(DateTime date)
-// 	{
-// 		UpdatedAt = date;
-// 	}
+class Entity
+{
+	public int Id { get; }
+	public DateTime CreatedAt;
+	public DateTime UpdatedAt { get; private set; }
+	public Entity()
+	{
+		Id = Random.Shared.Next(1, 1000000);
+		CreatedAt = DateTime.Now;
+	}
+	public void SetUpdatedAt(DateTime date)
+	{
+		UpdatedAt = date;
+	}
 
-// 	public override string ToString() => $"Id: {Id}, Skapad: {CreatedAt}, Uppdaterad: {UpdatedAt}";
-// }
-// class Contact : Entity
-// {
-// 	public string Name
-// 	{
-// 		get
-// 		{
-// 			return field;
-// 		}
-// 		set
-// 		{
-// 			SetUpdatedAt(DateTime.Now);
-// 			field = value;
-// 		}
-// 	}
-// 	public string Phone_Number
-// 	{
-// 		get => field;
-// 		set
-// 		{
-// 			SetUpdatedAt(DateTime.Now);
-// 			field = value;
-// 		}
-// 	}
+	public override string ToString() => $"Id: {Id}, Skapad: {CreatedAt}, Uppdaterad: {UpdatedAt}";
+}
+class Contact : Entity
+{
+	public string Name
+	{
+		get
+		{
+			return field;
+		}
+		set
+		{
+			SetUpdatedAt(DateTime.Now);
+			if (string.IsNullOrWhiteSpace(value)) throw new ArgumentException("Fältet får inte vara tomt");
+			else field = value;
+		}
+	}
+	public string Phone_Number
+	{
+		get => field;
+		set
+		{
+			SetUpdatedAt(DateTime.Now);
+			field = value;
+		}
+	}
 
-// 	public string GetPerson()
-// 	{
-// 		return Name + " - " + Phone_Number;
-// 	}
+	public string GetPerson()
+	{
+		return Name + " - " + Phone_Number;
+	}
 
-// 	public string ValidatePhoneNumber(string number)
-// 	{
-// 		string rawTimmy = number.Replace("+", string.Empty).Replace("-", string.Empty).Replace(" ", string.Empty).Trim();
-// 		if (rawTimmy.StartsWith("46"))
-// 			rawTimmy = string.Join(null, ["0", .. rawTimmy[2..]]);
+	public string ValidatePhoneNumber(string number)
+	{
+		string rawTimmy = number.Replace("+", string.Empty).Replace("-", string.Empty).Replace(" ", string.Empty).Trim();
+		if (rawTimmy.StartsWith("46"))
+			rawTimmy = string.Join(null, ["0", .. rawTimmy[2..]]);
 
-// 		if (!rawTimmy.StartsWith("07") || rawTimmy.Length != 10)
-// 		{
-// 			return "ERROR";
-// 		}
-// 		else
-// 		{
-// 			return rawTimmy;
-// 		}
-// 	}
+		if (!rawTimmy.StartsWith("07") || rawTimmy.Length != 10)
+		{
+			return "ERROR";
+		}
+		else
+		{
+			return rawTimmy;
+		}
+	}
 
-// 	public string FormatPhoneNumber(string num) => num.Insert(8, " ").Insert(6, " ").Insert(3, " - ");
+	public string FormatPhoneNumber(string num) => num.Insert(8, " ").Insert(6, " ").Insert(3, " - ");
 
-// 	public string ReverseName()
-// 	{
-// 		string name = string.Empty;
-// 		for (int i = Name.Length - 1; i >= 0; i--)
-// 		{
-// 			name += Name[i];
-// 		}
-// 		return name;
-// 	}
+	public string ReverseName()
+	{
+		string name = string.Empty;
+		for (int i = Name.Length - 1; i >= 0; i--)
+		{
+			name += Name[i];
+		}
+		return name;
+	}
 
-// 	public override string ToString() => $"Id: {Id}, Namn: {Name}, Telefonnummer: {Phone_Number}, Skapad: {CreatedAt}, Uppdaterad: {UpdatedAt}";
-// }
+	public override string ToString() => $"Id: {Id}, Namn: {Name}, Telefonnummer: {Phone_Number}, Skapad: {CreatedAt}, Uppdaterad: {UpdatedAt}";
+}
